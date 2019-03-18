1bef0e261e624fd654e4aaae3b2010a2e5b20ba3
Reinaldo <reinaldo.cardoso@youclap.tech>
2019-03-18 16:04:56 +0000

    fix all issues and optimize load files

--- working/assets/js/esport.js
+++ working/assets/js/esport.js
@@ -1,44 +1,6 @@
 (function($){
 	'use strict';
 
-	/*---------- LOADING EFFECT START ----------*/ 
-	setTimeout(function(){
-		$('body').addClass('loaded');
-	}, 2000);
-	/*---------- LOADING EFFECT END ----------*/
-
-	/*---------- SEARCH START ----------*/
-	$(function () {
-		$(document).on('click', '.header-search-content-wrapper>i', function(){
-			$('.header-search-content').addClass('header-search-form-active');
-			$('.header-search-content-wrapper').addClass('header-search-open');
-			$('.header-search-content-wrapper>i').attr('class', 'fa fa-search-minus');
-		});
-	
-		$(document).on('click', '.header-search-open i.fa-search-minus', function(){
-			$('.header-search-content-wrapper').removeClass('header-search-open');
-			$('.header-search-content-wrapper>i').attr('class', 'fa fa-search');
-			$('.header-search-content').removeClass('header-search-form-active');
-		});
-	});
-	/*---------- SEARCH END ----------*/
-
-	/*---------- SELECTS START ----------*/
-	$(document).ready(function() {
-		(function() {
-			[].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {	
-				new SelectFx(el);
-			} );
-		})();
-	});
-	/*---------- SELECTS END ----------*/
-
-	/*---------- TOOLTIP START ----------*/ 
-	$(function () {
-		$('[data-toggle="tooltip"]').tooltip()
-	});
-	/*---------- TOOLTIP END ----------*/
-
 	/*---------- MOBILE HEADER START ----------*/
 	$(function () {
 		$(document).on('click', '.mobile-header .mobile-menu-icon', function(){
@@ -89,79 +51,7 @@
 		});
 	});
 	/*---------- MOBILE HEADER END ----------*/
-
-	/*---------- POST FEATURED START ----------*/
-	var swiper = new Swiper('.post-featured-header-image-gallery', {
-		slidesPerView: 1,
-		slidesPerGroup: 1,
-		slidesPerColumn: 1,
-		spaceBetween: 0,
-		pagination: '.swiper-pagination',
-		paginationClickable: true,
-		nextButton: '.swiper-button-next',
-		prevButton: '.swiper-button-prev',
-		speed: 1000,
-		effect: 'fade'
-	});
-	/*---------- POST FEATURED END ----------*/
-
-	/*---------- ESPORT SLIDER AUTO HEIGHT START ----------*/
-	$(function(){
-		$(window).resize(function(){
-			var windowy = $(window).height();
-			var mobileHeaderHeight = $(".mobile-header").height();
-			var mobileHeaderHeight = windowy - mobileHeaderHeight;
-			$('.esport-slider-carousel .slider-wrapper').css('height',mobileHeaderHeight + 'px');
-		}).resize();    
-	});
-	/*---------- ESPORT SLIDER AUTO HEIGHT END ----------*/
-
-	/*---------- LOGO CAROUSEL START ----------*/
-	function gloriaSliders() {
-		$('.gloria-sliders').each( function() {
-			var cswiper = $(this),
-				autoplay = $(this).data('aplay'),
-				item = $(this).data('item'),
-				sloop = $(this).data('sloop'),
-				columnSpace = $(this).data('column-space'),
-				effect = $(this).data('effect'),
-				effectTime = $(this).data('effectTime'),
-				pagination = $(this).data('pagination');
-
-
-			var swiper = new Swiper(cswiper, {
-				slidesPerView: item,
-				autoplay: autoplay,
-				loop: sloop,
-				effect: effect,
-				spaceBetween: columnSpace,
-				nextButton: '.slider-next',
-				prevButton: '.slider-prev',
-				pagination: pagination,
-				paginationClickable: true,
-				preventClicks: false,
-				preventClicksPropagation: false,
-				breakpoints: {
-					991: {
-						slidesPerView: item < 6 ? item: 5, 
-					},
-					767: {
-						slidesPerView: item < 6 ? item: 4, 
-					},
-					550: {
-						slidesPerView: item < 6 ? item: 3, 
-					},
-					350: {
-						slidesPerView: item < 6 ? item: 2, 
-					},
-				}
-			});
-
-		});
-
-	}
-	gloriaSliders();
-	/*---------- LOGO CAROUSEL END ----------*/
+	
 
 	/*---------- TEAM & PLAYER START ----------*/ 
 	$(function () {
@@ -171,21 +61,5 @@
 		});
 	});
 	/*---------- TEAM & PLAYER END ----------*/
-
-	/*---------- FIXTURES START ----------*/ 
-	$(function () {
-		$( document ).ready(function() {
-			$('.fixtures-wrapper .tab-content .tab-pane:first-child').addClass('active');
-			$('.fixtures-wrapper .nav-tabs li:first-child').addClass('active');
-		});
-	});
-	/*---------- FIXTURES END ----------*/
-
-	/*---------- COUNTER START ----------*/
-	$('.esport-counter .number').counterUp({
-		delay: 10,
-		time: 2000
-	});
-	/*---------- COUNTER END ----------*/
 
 } )( jQuery );
---
+++ working/assets/js/ga.js
@@ -0,0 +1,9 @@
+function loadtracking() {
+    window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-104993254-6', {'anonymize_ip': true });
+}
+
+loadtracking();
+
